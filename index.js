03c02ff30554113ed975fa0b4c76bba7bb9e51ce
Wanessa Guedes <wanessa.o.guedes@gmail.com>
2022-05-26 16:26:55 -0300

    feat: CRUD games

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/categories", async (req,res) => {
     try{
         const  categorias = await connection.query("SELECT * FROM categories");
         //console.log("categorias do banco", categorias.rows);
-        res.sendStatus(200);
+        res.status(200).send(categorias.rows);
     } catch (e) {
         console.log(e);
         res.status(500).send("Ocorreu um erro ao obter as categorias");
@@ -67,6 +67,8 @@ app.post("/categories", async (req,res) => {
 //TODO: CRUD de Jogos [Create|Read]
 
 app.get("/games", async (req,res) => {
+    //TODO: Regras de Negócio
+    // Para a rota /games?name=ba, deve ser retornado uma array somente com os jogos que comecem com "ba", como "Banco Imobiliário", "Batalha Naval", etc
     try{
         const  games = await connection.query("SELECT * FROM games");
         console.log("jogos do banco", games.rows);
@@ -75,6 +77,54 @@ app.get("/games", async (req,res) => {
         console.log(e);
         res.status(500).send("Ocorreu um erro ao obter os jogos");
     }
+})
+
+app.post('/games', async (req,res) => {
+    //TODO: REGRAS DE NEGÓCIO
+    //`name` não pode estar vazio;
+    //`stockTotal` e `pricePerDay` devem ser maiores que 0; 
+    const dataGames = {
+        name: req.body.name,
+        stockTotal: req.body.stockTotal,
+        pricePerDay: req.body.pricePerDay
+    }
+    const schema = Joi.object({
+        name: Joi.string().required(),
+        stockTotal: Joi.number().integer().min(1).required(),
+        pricePerDay: Joi.number().integer().min(1).required()
+    })
+    const { error, value } = schema.validate(dataGames, {abortEarly: false});
+        
+    if(error){
+        return res.status(422).send(error.details.map(detail => detail.message));
+    }
+
+    //`categoryId` deve ser um id de categoria existente; ⇒ nesses casos, deve retornar **status 400**
+    const  categorias = await connection.query("SELECT * FROM categories");
+        let categorieExists = false;
+        for(let i = 0; i < categorias.rows.length; i++){
+            if(categorias.rows[i].id === req.body.categoryId){
+                categorieExists = true;
+            }
+        }
+        if(!categorieExists){
+            return res.status(400).send('Categoria não existente.');
+        }
+    //`name` não pode ser um nome de jogo já existente ⇒ nesse caso deve retornar **status 409**
+    const  games = await connection.query("SELECT * FROM games");
+        let gameExist = false;
+        for(let i = 0; i < games.rows.length; i++){
+            if(games.rows[i].name === req.body.name){
+                gameExist = true;
+            }
+        }
+        if(gameExist){
+            return res.status(409).send('Jogo já cadastrado.');
+        }
+
+        await connection.query(`INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay")
+                                VALUES ($1, $2, $3, $4, $5)`, [value.name, req.body.image, value.stockTotal, req.body.categoryId, value.pricePerDay]);
+        res.sendStatus(201);
 })
 
 // subindo back-end
